1ca547998917d5406e00521403fb3532efb51762
Fabrizio Franceschetti <fabrizio.franceschetti00@gmail.com>
2022-08-06 20:25:57 +0200

    Worked on Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,22 +2,25 @@ import React from 'react'
 import './Header.css' 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 
 function Header() {
     return (
         <div className='container'>
-            <div className='wrapper'>
-                <div className='left'>
-                    <img src='images/logo.png' alt="avatar"></img>
+
+                <div className='logo'>
+                    <img src='images/logo.png' alt="logo" className="logo" />
+                 </div>
+
+                <div className='cart'>
+                    <img src='images/cart.png' alt="cart" className="cart" />
                 </div>
-                <div className='right'>
-                    <img src='images/cart.png' alt="avatar"></img>
-                </div>
-            </div>
-            <hr className="solid"></hr>
         </div>
-    );
-}
+    )
+}    
 
 export default Header;
