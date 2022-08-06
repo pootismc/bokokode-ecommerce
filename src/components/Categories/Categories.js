b6507d4ab89bbc008f8eadd8d7138fe6a56ccfb3
Fabrizio Franceschetti <fabrizio.franceschetti00@gmail.com>
2022-08-06 10:44:55 +0200

    Categories component

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import './Categories.css' 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function Categories() {
+    return (
+        <div>
+            <h1>Categories</h1>
+        </div>
+    );
+}
+
+export default Categories;
