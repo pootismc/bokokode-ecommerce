c58e78d26b6dec24f472f8c1fc9cd80d9cca8a3e
Fabrizio Franceschetti <fabrizio.franceschetti00@gmail.com>
2022-08-06 10:44:11 +0200

    Featured product component

---
+++ src/components/FeaturedProduct/FeaturedProduct.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './FeaturedProduct.css' 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function FeaturedProduct() {
+    return (
+        <div className='container'>
+            <div className='hola'>
+                <h2>About the Samurai King Resting</h2>
+            </div>
+            
+             <div className='hola'>
+                <h3>Pets</h3>
+                <h4>So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Cicero's De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Cicero's De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.So how did the classical Latin become so incoherent? According to McClintock.</h4>
+            </div>
+        </div>
+        
+    );
+}
+
+export default FeaturedProduct;
