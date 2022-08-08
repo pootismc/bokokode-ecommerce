a862087acc60470f5bae9d29293d41563c55ad29
Fabrizio Franceschetti <fabrizio.franceschetti00@gmail.com>
2022-08-08 08:39:56 +0200

    Added array of people also buy products

--- src/components/FeaturedProduct/FeaturedProduct.js
+++ src/components/FeaturedProduct/FeaturedProduct.js
@@ -5,6 +5,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function FeaturedProduct() {
+    const alsoBuy = [
+        {
+            name: 'Egg Balloon',
+            type: 'Food',
+            price: '$93.89',
+            image: 'images/yellow.png'
+        },
+        {
+            name: 'Egg Balloon',
+            type: 'Food',
+            price: '$93.89',
+            image: 'images/window.png'
+        },
+        {
+            name: 'Egg Balloon',
+            type: 'Food',
+            price: '$93.89',
+            image: 'images/eggballoon.png',
+        }
+    ]
+    const productList = alsoBuy.map(product => ( <img src={product.image} className='alsoimg' alt='alsobuy'/>))
+
+    const productList1 = alsoBuy.map(product => ( <h3 className='type1'>{product.type}</h3>))
+    
+    const productList2 = alsoBuy.map(product => ( <h2 className='tittle1'>{product.name}</h2>))
+    
+    const productList3 = alsoBuy.map(product => ( <h2 className='price1'>{product.price}</h2>))
+
     return (
         <div className='containerProd'>
 
@@ -24,39 +52,30 @@ function FeaturedProduct() {
             </div>
 
             <div className='row1'>
-            <div className='test2'>
-                    <h2 className='prodTittle'>About the Samurai King Resting</h2>
-                    <h3 className='prodType'>Pets</h3>
-                    <h4 className='prodDes'> So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Cicero's De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Cicero's De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.So how did the classical Latin become so incoherent? According to McClintock.</h4>
-            </div>
+                <div className='test2'>
+                        <h2 className='prodTittle'>About the Samurai King Resting</h2>
+                        <h3 className='prodType'>Pets</h3>
+                        <h4 className='prodDes'> So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Cicero's De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Cicero's De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.So how did the classical Latin become so incoherent? According to McClintock.</h4>
+                </div>
 
-            <div className='padding'>
-                     <h2 className='text'>People also buy</h2>
-                     <p></p>
-                     <div className='test3'>
+                <div className='padding'>
+                <h2 className='text'>People also buy</h2>
+                    <div className='test3'>
+                        <div className='' > 
+                            {productList}
+                        </div>
                         <div className=''> 
-                            <img src='images/eggballoon.png' className='alsoimg' alt='alsobuy'/>
-                            <h3 className='type1'>Food</h3>
-                        <h2 className='tittle1'>Egg Balloon</h2>
-                         <h3 className='price1'>$93.89 </h3>
+                            {productList1}
                         </div>
-                        <div> 
-                            <img src='images/eggballoon.png' className='alsoimg' alt='alsobuy'/>
-                            <h3 className='type1'>Food</h3>
-                        <h2 className='tittle1'>Egg Balloon</h2>
-                         <h3 className='price1'>$93.89 </h3>
+                        <div className=''> 
+                            {productList2}
                         </div>
-                        <div> 
-                            <img src='images/eggballoon.png' className='alsoimg' alt='alsobuy'/>
-                            <h3 className='type1'>Food</h3>
-                        <h2 className='tittle1'>Egg Balloon</h2>
-                         <h3 className='price1'>$93.89 </h3>
+                        <div className=''> 
+                            {productList3}
                         </div>
-
-                    
-                         </div>
+                    </div>
             </div>          
-            </div> 
+        </div> 
 
                     
 
