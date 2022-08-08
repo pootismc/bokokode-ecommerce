ce07ea5be6843ed84b3dc27e9f5fc863fe6d11fa
Fabrizio Franceschetti <fabrizio.franceschetti00@gmail.com>
2022-08-08 08:40:15 +0200

    Deleted Products component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import Header from "./components/Header/Header";
 // import Categories from "./components/Categories/Categories";
 import FeaturedProduct from "./components/FeaturedProduct/FeaturedProduct";
 import Product from "./components/Product/Product";
-
-import Products from "./components/Products/Products";
-
 import LandingPagination from "./components/Pagination/Pagination";
+
 
 
 function App() {
